dbaf700046393994c2cddd955df6494b3dd32549
Sean Day <Asarlis55@gmail.com>
2021-04-20 21:38:22 +0000

    middleTest refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,12 @@
 const middle = require('../middle');
+const assert = require('chai').assert;
+
+describe('#middle', () => {
+  it('should return [2, 3] when passed [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])  
+  });
+  it('should return [2] when passed [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])  
+  });
+  
+});
