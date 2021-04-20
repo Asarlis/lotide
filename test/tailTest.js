26a3bc7d0e111d12f49cee818be3a172b3fbcd60
Sean Day <Asarlis55@gmail.com>
2021-04-20 20:42:21 +0000

    added tailTest mocha chai refactor

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const tail        = require('../tail');
+const assert      = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail", () =>{
+  it("should return [1] when given [0, 1]", () => {
+    assert.deepEqual(tail([0, 1]), [1])
+  });
+  it("should return [1, 2, 3] when given [0, 1, 2, 3]", () => {
+    assert.deepEqual(tail([0, 1, 2, 3]), [1, 2, 3])
+  });
+});
