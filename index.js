9b8152475c1d3290a6a37fabf517f056b384e51b
Sean Day <Asarlis55@gmail.com>
2021-04-22 00:45:21 +0000

    populated index keys

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 //index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
+const assertArraysEqual  = require('./assertArraysEqual');
+const assertEqual        = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays           = require('./eqArrays');
+const eqObjects          = require('./eqObjects');
+const findKey            = require('./findKey'); 
+const head               = require('./head');
+const main               = require('./main');
+const map                = require('./map');
+const middle             = require('./middle');
+const moduleCheck        = require('./moduleCheck');
+const tail               = require('./tail');
+const takeUntil          = require('./takeUntil');
+const without            = require('./without'); 
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-
+  assertArraysEqual:  assertArraysEqual,
+  assertEqual:        assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqArrays:           eqArrays,
+  eqObjects:          eqObjects,
+  findKey:            findKey,
+  head:               head,
+  main:               main,
+  map:                map,
+  middle:             middle,
+  moduleCheck:        moduleCheck,
+  tail:               tail,
+  takeUntil:          takeUntil,
+  without:            without,
 };
